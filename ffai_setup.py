298fa9d0e60d2fb2a29c2f5589076cc27aeddd33
prsolans <prsolans@gmail.com>
2019-06-13 20:47:16 -0500

    Updated setup file

--- ffai_setup.py
+++ ffai_setup.py
@@ -5,8 +5,8 @@ from sources.pro_football_reference_fantasy_points import get_pfr_fantasy_data
 from utilities.utility_data import create_tables
 
 if __name__ == '__main__':
-    # create_tables()
-    # get_team_data()
-    # get_player_data()
-    # get_nfl_com_data()
+    create_tables()
+    get_team_data()
+    get_player_data()
+    get_nfl_com_data()
     get_pfr_fantasy_data()
