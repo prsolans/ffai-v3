1ad9ca46dfb2605ee83a5dec949a098ab9a00a13
Paul R. Solans <prsolans@gmail.com>
2019-07-01 18:41:45 -0500

    Created postgresql connection, loop through teams

--- ffai_setup.py
+++ ffai_setup.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
 import os
 import logging
+import psycopg2
 
 from sources.source_teams import get_team_data
 from sources.source_players import get_player_data
 from sources.nflcom_fantasy_points import get_nfl_com_data
 from sources.pro_football_reference_fantasy_points import get_pfr_fantasy_data
-from utilities.utility_data import create_tables
+from utilities.utility_data import create_postgresql_connection, get_teams, get_team
 
 app = Flask(__name__)
 
@@ -19,17 +20,24 @@ def index():
     # get_pfr_fantasy_data()
     return render_template('index.html')
 
-@app.route('/player/<playerid>')
-def player(playerid=''):
-    return render_template('player.html', playerid=playerid)
 
 @app.route('/players')
 def players():
     return render_template('players.html')
 
+@app.route('/player/<playerid>')
+def player(playerid=''):
+    return render_template('player.html', playerid=playerid)
+
 @app.route('/teams')
 def teams():
-    return render_template('teams.html')
+    teams = get_teams()
+    return render_template('teams.html', teams=teams)
+
+@app.route('/team/<teamid>')
+def team(teamid=''):
+    team = get_team(teamid)
+    return render_template('team.html', team=team)
 
 if __name__ == '__main__':
     app.run()
--- utilities/utility_data.py
+++ utilities/utility_data.py
@@ -1,6 +1,54 @@
 import sqlite3
 from sqlite3 import Error
 import re
+import psycopg2
+
+def create_postgresql_connection():
+    try:
+        connection = psycopg2.connect(user = "prsolans",
+                                    password = "",
+                                    host = "127.0.0.1",
+                                    port = "5432",
+                                    database = "ffai")
+        cursor = connection.cursor()
+        # Print PostgreSQL Connection properties
+        print ( connection.get_dsn_parameters(),"\n")
+        # Print PostgreSQL version
+        cursor.execute("SELECT version();")
+        record = cursor.fetchone()
+        print("You are connected to - ", record,"\n")
+        return(cursor)
+    except (Exception, psycopg2.Error) as error :
+        print ("Error while connecting to PostgreSQL", error)
+
+def get_teams():
+    try:
+        cursor = create_postgresql_connection()
+        cursor.execute("SELECT * from teams;")
+        rows = cursor.fetchall()
+        teams = []
+        for row in rows:
+            teams.append(row)
+        print(type(teams))
+        return(teams)
+    except (Exception, psycopg2.Error) as error :
+        print ("Error while connecting to PostgreSQL", error)
+    # finally:
+        #closing database connection.
+            # if(connection):
+            #     cursor.close()
+            #     connection.close()
+            #     print("PostgreSQL connection is closed")
+
+def get_team(id):
+    try:
+        cursor = create_postgresql_connection()
+        cursor.execute("SELECT * from teams WHERE team_id="+id+";")
+        team = cursor.fetchone()
+        print(team)
+        return(team)
+    except (Exception, psycopg2.Error) as error :
+        print ("Error while connecting to PostgreSQL", error)
 
 def create_connection():
     """ create a database connection to a database that resides
@@ -11,6 +59,7 @@ def create_connection():
         return(conn)
     except Error as e:
         print(e)
+
 
 def create_tables():
     """ create core database tables
@@ -45,7 +94,7 @@ def insert_data(data):
         conn = create_connection()
         c = conn.cursor()
         c.execute(data)
-        conn.commit()        
+        conn.commit()
     except Error as e:
         print(e)
     finally:
@@ -58,7 +107,7 @@ def convert_to_integer(input):
     try:
         intValue = re.sub('[^0-9]','', input)
         if len(intValue) < 1:
-            intValue = '0'    
+            intValue = '0'
         intValue = int(intValue)
         return intValue
     except Error as e:
